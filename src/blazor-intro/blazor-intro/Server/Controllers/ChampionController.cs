ecff0963148d83d0f65752b1cf21f291eaa69268
Abdullah ILGAZ <mail@abdullahilgaz.com>
2021-02-15 22:39:56 +0300

    wip

---
+++ src/blazor-intro/blazor-intro/Server/Controllers/ChampionController.cs
@@ -0,0 +1,282 @@
+﻿using blazor_intro.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazor_intro.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChampionController : ControllerBase
+    {
+        private readonly ILogger<ChampionController> logger;
+
+        public ChampionController(ILogger<ChampionController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Champion> Get()
+        {
+            List<Champion> champs = new List<Champion>()
+            {
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+                new Champion()
+                {
+                    Year = 2019,
+                    Team = "Toronto Raptors"
+                },
+                new Champion()
+                {
+                    Year = 2020,
+                    Team = "Los Angeles Lakers"
+                },
+            };
+
+            return champs.ToArray();
+        }
+    }
+}
---
+++ src/blazor-intro/blazor-intro/Shared/Champion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace blazor_intro.Shared
+{
+    public class Champion
+    {
+        public int Year { get; set; }
+
+        public string Team { get; set; }
+
+    }
+}
